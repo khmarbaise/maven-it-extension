7f5e58d48cbeef7ece3ba00ca641211b69a4a131
Karl Heinz Marbaise <khmarbaise@apache.org>
2019-10-27 23:21:42 +0100

    Improved code.

--- src/test/java/org/apache/maven/jupiter/it/EARIT.java
+++ src/test/java/org/apache/maven/jupiter/it/EARIT.java
@@ -40,7 +40,8 @@ import org.apache.maven.jupiter.extension.maven.MavenProjectResult;
  *
  * @author Karl Heinz Marbaise
  */
-@MavenIT class EARIT {
+@MavenIT
+class EARIT {
 
   @MavenTest
   void basic(MavenExecutionResult result, MavenProjectResult project) {
