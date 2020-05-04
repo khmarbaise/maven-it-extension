438f62c8cf45476ff43633b13c117932e2ea9b9a
khmarbaise <github@soebes.de>
2020-05-04 20:07:56 +0000

    Deploying to gh-pages from  @ 7c53a3fbde3bac291bb68b5c8f278cc4d73e8803 🚀

---
+++ snapshot/itf-maven-plugin/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.soebes.itf.maven.plugin","l":"HelpMojo"},{"p":"com.soebes.itf.maven.plugin","l":"InstallMojo"}]
---
+++ snapshot/itf-maven-plugin/testapidocs/javadoc.sh
@@ -0,0 +1,1 @@
+/opt/hostedtoolcache/jdk/11.0.7/x64/bin/javadoc @options @packages
