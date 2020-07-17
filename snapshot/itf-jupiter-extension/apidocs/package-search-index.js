44319fc8dcb1d58d0eb4469260934ad05d37aece
khmarbaise <github@soebes.de>
2020-07-17 21:54:27 +0000

    Deploying to gh-pages from  @ 10af0feebd4401013429ae1422368a7038a21f40 🚀

--- snapshot/itf-jupiter-extension/apidocs/package-search-index.js
+++ snapshot/itf-jupiter-extension/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.soebes.itf.jupiter.extension"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.soebes.itf.jupiter.extension"},{"l":"com.soebes.itf.jupiter.extension.condition"}]
