ec788e4b1a61da5dfb86919db6dc55d92339f3e4
khmarbaise <github@soebes.de>
2020-08-12 08:14:26 +0000

    Deploying to gh-pages from  @ d80ab6549e2f81733536d3a69d8ad79ac9a256e6 🚀

---
+++ snapshot/itf-examples/testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.soebes.itf.examples.goals","l":"GoalsCleanVerify"},{"p":"com.soebes.itf.examples.profiles","l":"ProfilesOneTwoAndThree"},{"p":"com.soebes.itf.examples.properties","l":"SkipTestAnnotation"}]
