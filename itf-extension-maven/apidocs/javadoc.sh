dd24260566b9d6c516387583eeac29495183242c
khmarbaise <github@soebes.de>
2022-12-29 21:33:29 +0000

    Deploying to gh-pages from @ khmarbaise/maven-it-extension@8252a366b5be9bab2fd78e8f20b8c0b19fa40048 🚀

--- itf-extension-maven/apidocs/javadoc.sh
+++ itf-extension-maven/apidocs/javadoc.sh
@@ -1,1 +1,1 @@
-/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/19.0.1-10/x64/bin/javadoc @options @packages
+/opt/hostedtoolcache/Java_Temurin-Hotspot_jdk/18.0.2-101/x64/bin/javadoc @options @packages
