e27f33cd5ec7ad3dddc6aa885d4528f65c8b36ba
Karl Heinz Marbaise <kama@soebes.de>
2023-04-16 19:48:30 +0200

    Added language comments for IntelliJ configuration.

--- itf-examples/src/test/java/com/soebes/itf/examples/mps/MavenProjectSourcesMoreComplexIT.java
+++ itf-examples/src/test/java/com/soebes/itf/examples/mps/MavenProjectSourcesMoreComplexIT.java
@@ -47,6 +47,7 @@ import static com.soebes.itf.jupiter.extension.MavenProjectSources.ResourceUsage
 @MavenProjectSources(resourcesUsage = NONE)
 class MavenProjectSourcesMoreComplexIT {
 
+  //language=XML
   private String generateProject(String info) {
     return """
            <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/maven-v4_0_0.xsd">
